53814c33e5d927f480aebe01c46993b0704170d6
Z-Mobile <h.zante2011@gmail.com>
2024-06-04 10:34:55 -0700

    Fixed mixin server player net handler

--- src/launch/java/baritone/launch/mixins/MixinServerPlayNetHandler.java
+++ src/launch/java/baritone/launch/mixins/MixinServerPlayNetHandler.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of Baritone.
- *
- * Baritone is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Baritone is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package baritone.launch.mixins;
 
 import baritone.Baritone;
@@ -44,23 +27,6 @@ import java.util.List;
  */
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class MixinServerPlayNetHandler {
-
-    /*@Inject(
-            method = "handleChat(Lnet/minecraft/network/protocol/game/ServerboundChatPacket;)V",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private void onChatMessage(ServerboundChatPacket packet, CallbackInfo ci) {
-        String message = packet.getMessage();
-        ChatEvent event = new ChatEvent(message);
-        for (IBaritone baritone : BaritoneAPI.getProvider().getAllBaritones()) {
-            baritone.getGameEventHandler().onSendChatMessage(event);
-            if (event.isCancelled()) {
-                ci.cancel();
-                break;
-            }
-        }
-    }*/
 
     @Inject(
             method = "handleLevelChunkWithLight",
